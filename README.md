50921d5924b3d73e2f6144d74bebee5e72fbeebd
Zairochess Psudo <zairochess@gmail.com>
2022-01-29 21:11:45 +0800

    add wordle solver v1.1

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # wordle-solver
 
 Words file contains dictionary from Ubuntu /usr/share/dict/words.
+To run the program, # bash wordle.bash
--- wordle.bash
+++ wordle.bash
@@ -2,17 +2,21 @@
 
 PATH_SRC=~/Downloads/wordle-solver
 function _guess {
-	name=guess
-
 	clear
 	echo " ****************************************************************"
-	echo "               			WORDLE SOLVER                          "
+	echo "               		   WORDLE SOLVER                           "
 	echo " ****************************************************************"
 
 	# 1. length
-	read -p " Length (2-46): [5] " length
+	read -p " [1 of 4] Length (2-46): [5] " length
 	length=${length:-5}
-	if [[ $length == "" ]]
+
+	if [[ -n ${length//[0-9]/} ]]
+	then
+		exit
+	fi
+
+	if (( length < 2 || length > 46 ))
 	then
 		exit
 	fi
@@ -28,22 +32,35 @@ function _guess {
 	grep "^$pattern$" $PATH_SRC/words > $PATH_SRC/word
 	
 	# 2. exclude
-	read -p " Exclude: " exclude
+	read -p " [2 of 4] Exclude: " exclude
 	if [[ $exclude != "" ]]
 	then
 		grep -v "[$exclude]" $PATH_SRC/word > $PATH_SRC/word2
 		cp $PATH_SRC/word2 $PATH_SRC/word
 	fi
 
-	# 3. include
-	read -p " Include Pattern (e.g. .oun.): " include
+	# 3. contains
+	read -p " [3 of 4] Include: " include
 	if [[ $include != "" ]]
 	then
-		grep "$include" $PATH_SRC/word > $PATH_SRC/word2
+		for (( i = 0; i < ${#include}; i++ ))
+		do
+			grep "${include:$i:1}" $PATH_SRC/word > $PATH_SRC/word2
+			cp $PATH_SRC/word2 $PATH_SRC/word
+		done
+	fi
+
+	# 4. include
+	read -p " [4 of 4] Include Pattern (e.g. .oun.): " pattern2
+	if [[ $pattern2 != "" ]]
+	then
+		grep "$pattern2" $PATH_SRC/word > $PATH_SRC/word2
 		cp $PATH_SRC/word2 $PATH_SRC/word
 	fi
 
-	cat $PATH_SRC/word
+	echo ""
+	sort -R $PATH_SRC/word | head -n5
+	echo ""
 }
 
 _guess
