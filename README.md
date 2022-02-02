bc4100d8dcd9474d3cfff63c1a907f459fb851c9
Zairochess Psudo <zairochess@gmail.com>
2022-02-02 22:39:30 +0800

    add wordle solver v1.2

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
 # wordle-solver
 
-Words file contains dictionary from Ubuntu /usr/share/dict/words.
-To run the program, # bash wordle.bash
+Dictionary data 
+    words (102,401) from Ubuntu /usr/share/dict/words
+    words_scrabble (172,820 words) from https://raw.githubusercontent.com/zeisler/scrabble/master/db/dictionary.csv
+
+To run the program
+    # bash wordle.bash
+
+ e.g.
+ ****************************************************************
+               		   WORDLE SOLVER                           
+ ****************************************************************
+ [1 of 5] Length (2-46): [5] 
+ [2 of 5] Exclude: rtyuiopsdglcvbnm
+ [3 of 5] Exclude Pattern (e.g. .i..s): 
+ [4 of 5] Include: kae
+ [5 of 5] Include Pattern (e.g. .oun.): ..a.e
+
+awake
+
--- wordle.bash
+++ wordle.bash
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 PATH_SRC=~/Downloads/wordle-solver
+DICTIONARY=words_scrabble
 function _guess {
 	clear
 	echo " ****************************************************************"
@@ -8,7 +9,7 @@ function _guess {
 	echo " ****************************************************************"
 
 	# 1. length
-	read -p " [1 of 4] Length (2-46): [5] " length
+	read -p " [1 of 5] Length (2-46): [5] " length
 	length=${length:-5}
 
 	if [[ -n ${length//[0-9]/} ]]
@@ -29,18 +30,26 @@ function _guess {
 
 	touch $PATH_SRC/word
 	touch $PATH_SRC/word2
-	grep "^$pattern$" $PATH_SRC/words > $PATH_SRC/word
+	grep "^$pattern$" $PATH_SRC/$DICTIONARY > $PATH_SRC/word
 	
 	# 2. exclude
-	read -p " [2 of 4] Exclude: " exclude
+	read -p " [2 of 5] Exclude: " exclude
 	if [[ $exclude != "" ]]
 	then
 		grep -v "[$exclude]" $PATH_SRC/word > $PATH_SRC/word2
 		cp $PATH_SRC/word2 $PATH_SRC/word
 	fi
 
-	# 3. contains
-	read -p " [3 of 4] Include: " include
+	# 3. exclude pattern
+	read -p " [3 of 5] Exclude Pattern (e.g. .i..s): " exclude_pattern
+	if [[ $exclude_pattern != "" ]]
+	then
+		grep -v "$exclude_pattern" $PATH_SRC/word > $PATH_SRC/word2
+		cp $PATH_SRC/word2 $PATH_SRC/word
+	fi
+
+	# 4. include
+	read -p " [4 of 5] Include: " include
 	if [[ $include != "" ]]
 	then
 		for (( i = 0; i < ${#include}; i++ ))
@@ -50,11 +59,11 @@ function _guess {
 		done
 	fi
 
-	# 4. include
-	read -p " [4 of 4] Include Pattern (e.g. .oun.): " pattern2
-	if [[ $pattern2 != "" ]]
+	# 5. include pattern
+	read -p " [5 of 5] Include Pattern (e.g. .oun.): " include_pattern
+	if [[ $include_pattern != "" ]]
 	then
-		grep "$pattern2" $PATH_SRC/word > $PATH_SRC/word2
+		grep "$include_pattern" $PATH_SRC/word > $PATH_SRC/word2
 		cp $PATH_SRC/word2 $PATH_SRC/word
 	fi
 
