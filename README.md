78918bdd636b3a0f84f3440f1f0a7cf4394bb75e
Zairochess Psudo <zairochess@gmail.com>
2022-02-02 23:19:09 +0800

    Adds menu

--- README.md
+++ README.md
@@ -6,16 +6,3 @@ Dictionary data
 
 To run the program
     # bash wordle.bash
-
- e.g.
- ****************************************************************
-               		   WORDLE SOLVER                           
- ****************************************************************
- [1 of 5] Length (2-46): [5] 
- [2 of 5] Exclude: rtyuiopsdglcvbnm
- [3 of 5] Exclude Pattern (e.g. .i..s): 
- [4 of 5] Include: kae
- [5 of 5] Include Pattern (e.g. .oun.): ..a.e
-
-awake
-
--- wordle.bash
+++ wordle.bash
@@ -2,74 +2,135 @@
 
 PATH_SRC=~/Downloads/wordle-solver
 DICTIONARY=words_scrabble
-function _guess {
-	clear
+function _menu {
+	name=menu
+
 	echo " ****************************************************************"
 	echo "               		   WORDLE SOLVER                           "
 	echo " ****************************************************************"
 
-	# 1. length
-	read -p " [1 of 5] Length (2-46): [5] " length
-	length=${length:-5}
+	echo " 0 Exit "
+	echo " "		
 
-	if [[ -n ${length//[0-9]/} ]]
-	then
-		exit
-	fi
+	echo " 1 New"
+	echo " 2 Exclude"
+	echo " 3 Exclude Pattern"
+	echo " 4 Include"
+	echo " 5 Include Pattern"
+	echo " "	
+	read -rp " [$name] Please select an option: " inputMenu
 
-	if (( length < 2 || length > 46 ))
-	then
-		exit
-	fi
+	case $inputMenu in 
+		0|00)
+			clear
+			exit
+		;;
+
+		1)
+			# length
+			read -p " [1 of 5] Length (2-46): [5] " length
+			length=${length:-5}
+
+			if [[ -n ${length//[0-9]/} ]]
+			then
+				exit
+			fi
+
+			if (( length < 2 || length > 46 ))
+			then
+				exit
+			fi
+			
+			pattern=''
+			str='[a-z]'
+			for (( i = 1; i <= $length; i++ )) 
+			do
+				pattern="${pattern}${str}"
+			done
+
+			touch $PATH_SRC/word
+			touch $PATH_SRC/word2
+			grep "^$pattern$" $PATH_SRC/$DICTIONARY > $PATH_SRC/word
+
+			echo ""
+			sort -R $PATH_SRC/word | head -n10
+			echo ""
+
+			_menu
+		;;
+
+		2)
+			# exclude
+			read -p " [2 of 5] Exclude: " exclude
+			if [[ $exclude != "" ]]
+			then
+				grep -v "[$exclude]" $PATH_SRC/word > $PATH_SRC/word2
+				cp $PATH_SRC/word2 $PATH_SRC/word
+			fi
+
+			echo ""
+			sort -R $PATH_SRC/word | head -n10
+			echo ""
+
+			_menu
+		;;
+
+		3)
+			# exclude pattern
+			read -p " [3 of 5] Exclude Pattern (e.g. .i..s): " exclude_pattern
+			if [[ $exclude_pattern != "" ]]
+			then
+				grep -v "$exclude_pattern" $PATH_SRC/word > $PATH_SRC/word2
+				cp $PATH_SRC/word2 $PATH_SRC/word
+			fi
+
+			echo ""
+			sort -R $PATH_SRC/word | head -n10
+			echo ""
+
+			_menu
+		;;
+
+		4)
+			# include
+			read -p " [4 of 5] Include: " include
+			if [[ $include != "" ]]
+			then
+				for (( i = 0; i < ${#include}; i++ ))
+				do
+					grep "${include:$i:1}" $PATH_SRC/word > $PATH_SRC/word2
+					cp $PATH_SRC/word2 $PATH_SRC/word
+				done
+			fi
+
+			echo ""
+			sort -R $PATH_SRC/word | head -n10
+			echo ""
+
+			_menu
+		;;
+
+		5)
+			# include pattern
+			read -p " [5 of 5] Include Pattern (e.g. .oun.): " include_pattern
+			if [[ $include_pattern != "" ]]
+			then
+				grep "$include_pattern" $PATH_SRC/word > $PATH_SRC/word2
+				cp $PATH_SRC/word2 $PATH_SRC/word
+			fi
+
+			echo ""
+			sort -R $PATH_SRC/word | head -n10
+			echo ""
+
+			_menu
+		;;
+
+		*) 
+		_menu
+		;;
 	
-	str='[a-z]'
-	for (( i = 1; i <= $length; i++ )) 
-	do
-		pattern="${pattern}${str}"
-	done
-
-	touch $PATH_SRC/word
-	touch $PATH_SRC/word2
-	grep "^$pattern$" $PATH_SRC/$DICTIONARY > $PATH_SRC/word
-	
-	# 2. exclude
-	read -p " [2 of 5] Exclude: " exclude
-	if [[ $exclude != "" ]]
-	then
-		grep -v "[$exclude]" $PATH_SRC/word > $PATH_SRC/word2
-		cp $PATH_SRC/word2 $PATH_SRC/word
-	fi
-
-	# 3. exclude pattern
-	read -p " [3 of 5] Exclude Pattern (e.g. .i..s): " exclude_pattern
-	if [[ $exclude_pattern != "" ]]
-	then
-		grep -v "$exclude_pattern" $PATH_SRC/word > $PATH_SRC/word2
-		cp $PATH_SRC/word2 $PATH_SRC/word
-	fi
-
-	# 4. include
-	read -p " [4 of 5] Include: " include
-	if [[ $include != "" ]]
-	then
-		for (( i = 0; i < ${#include}; i++ ))
-		do
-			grep "${include:$i:1}" $PATH_SRC/word > $PATH_SRC/word2
-			cp $PATH_SRC/word2 $PATH_SRC/word
-		done
-	fi
-
-	# 5. include pattern
-	read -p " [5 of 5] Include Pattern (e.g. .oun.): " include_pattern
-	if [[ $include_pattern != "" ]]
-	then
-		grep "$include_pattern" $PATH_SRC/word > $PATH_SRC/word2
-		cp $PATH_SRC/word2 $PATH_SRC/word
-	fi
-
-	echo ""
-	sort -R $PATH_SRC/word | head -n5
-	echo ""
+	esac
 }
 
-_guess
+_menu
